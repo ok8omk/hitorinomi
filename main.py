570089c9ad2703adc4f4f1cf50f0134d0daf1055
Tomoki Oke <tomoki.oke@gmail.com>
2017-09-09 15:03:57 +0900

    add debug mode

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template()
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True # デバッグモード有効化
+    app.run(host='0.0.0.0') # どこからでもアクセス可能に
